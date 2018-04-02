a9d0ff1709c22de1c6464bbee68318c6b3df4ece
Ivan Cedeño <icedeno@gmail.com>
2018-04-02 15:58:17 -0400

    VisitorsApp business layer created.

---
+++ UUSA.Tests/PersonTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UUSA.Biz;
+
+namespace UUSA.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void PersonTest()
+        {
+            //Arrange
+            var person = new Person
+            {
+                FirstName = "Ivan",
+                LastName = "Cedeno"
+            };
+            var expected = "Ivan Cedeno";
+
+            //Act
+            var actual = person.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UUSA.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UUSA.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UUSA.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b51c99f9-f914-41ed-9f89-539d475f2fe4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
