3eb3243a68edb166f9dcfa4c089727fa1886d1df
Ivan Cedeño <icedeno@gmail.com>
2018-04-03 19:56:58 -0400

    Unit testing for Visit class.

--- UUSA.Biz/Visit.cs
+++ UUSA.Biz/Visit.cs
@@ -11,12 +11,27 @@ namespace UUSA.Biz
     /// </summary>
     public class Visit
     {
+        #region Fields
+
+        private DateTime? arrival;
+        private DateTime? departure;
+
+        #endregion
+
         #region Properties
 
         public VisitorHost Host { get; set; }
         public List<Visitor> Visitors { get; set; }
-        public DateTime Arrival { get; set; }
-        public DateTime Departure { get; set; }
+        public DateTime Arrival
+        {
+            get { return arrival ?? DateTime.Now; }
+            set { arrival = value; }
+        }
+        public DateTime Departure
+        {
+            get { return departure ?? DateTime.Now; }
+            set { departure = value; }
+        }
 
         #endregion
     }
---
+++ UUSA.Tests/VisitTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UUSA.Biz;
+
+namespace UUSA.Tests
+{
+    [TestClass]
+    public class VisitTests
+    {
+        /// <summary>
+        /// If Null, then the Arrival and Departure Properties of Visit will
+        /// return the current DateTime.
+        /// </summary>
+        [TestMethod]
+        public void ArrivalAndDepartureNullTest()
+        {
+            //Arrange
+            var visit = new Visit();
+            var expected = DateTime.Today;
+
+            //Act
+            var actual_arrival = visit.Arrival.Date;
+            var actual_departure = visit.Departure.Date;
+
+            //Assert
+            Assert.AreEqual(expected, actual_arrival);
+            Assert.AreEqual(expected, actual_departure);
+        }
+
+        /// <summary>
+        /// Checking to see if by default the value of Host in an instance of Visit
+        /// is null.
+        /// </summary>
+        [TestMethod]
+        public void TestHostIsNullByDefault()
+        {
+            //Arrange
+            var visit = new Visit();
+
+            //Act
+            var actual = visit.Host;
+
+            //Assert
+            Assert.IsNull(actual);
+        }
+
+        /// <summary>
+        /// This test will confirm that Visitors list is null by default.
+        /// </summary>
+        [TestMethod]
+        public void TestVisitorsListIsNullByDefault()
+        {
+            //Arrange
+            var visit = new Visit();
+
+            //Act
+            var actual = visit.Visitors;
+
+            //Assert
+            Assert.IsNull(actual);
+        }
+
+        /// <summary>
+        /// Test will confirm an instance of a Visit object.
+        /// </summary>
+        [TestMethod]
+        public void TestInstanceOfVisit()
+        {
+            //Arrange
+            var visit = new Visit
+            {
+                Host = new VisitorHost
+                {
+                    FirstName = "Ivan",
+                    LastName = "Cedeno",
+                    PhoneNumber = "2129222604"
+                },
+                Visitors = new List<Visitor>
+                {
+                    new Visitor
+                    {
+                        FirstName = "Bernie",
+                        LastName = "Cedeno",
+                        CompanyName = "Brooklyyn Rep"
+                    },
+                    new Visitor
+                    {
+                        FirstName = "Lauren",
+                        LastName = "Savage",
+                        CompanyName = "14StreetY"
+                    }
+                },
+                Arrival = DateTime.Now.AddDays(1),
+                Departure = DateTime.Now.AddDays(1).AddHours(2)
+            };
+            var expectedHost = "Ivan Cedeno";
+            var expectedVisitorName = "Lauren Savage";
+
+            //Act
+            var actualHost = visit.Host.FullName;
+            var actualVisitor = visit.Visitors.Find
+                (v => v.FullName == "Lauren Savage");
+
+            //Assert
+            Assert.AreEqual(expectedHost, actualHost);
+            Assert.AreEqual(expectedVisitorName, actualVisitor.FullName);
+        }
+    }
+}
